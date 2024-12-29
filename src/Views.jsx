4b0e5072226ce4caa5358ce45d1ee2026f0958b7
Aditya kumar <145295567+adityspecial@users.noreply.github.com>
2024-12-30 01:10:57 +0530

    updated with backend

--- src/Views.jsx
+++ src/Views.jsx
@@ -4,6 +4,8 @@ import Home from "./Pages/Home/Home"
 import Events from "./Pages/Events/Events"
 import Team from "./Pages/Team/Team"
 import Cultural from "./Pages/Cultural/Cultural"
+import LoginSignupPage from "./Pages/Login/login"
+import FormPage from "./Pages/Forms/Form"
 
 const Views = () => {
   return (
@@ -13,6 +15,9 @@ const Views = () => {
         <Route path="events" element={<Events />} />
         <Route path="team" element={<Team />} />
         <Route path="cultural" element={<Cultural />} />
+        <Route path="login" element={<LoginSignupPage/>} />
+        <Route path="forms" element={<FormPage/>} />
+
       </Route>
       <Route index element={<Navigate to="/home" replace />} />
     </Routes>
