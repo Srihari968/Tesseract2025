3e0b75789a9bba07730f0454a2129b72151ed2ab
Varshi18 <kamatham.varshith@gmail.com>
2025-01-04 12:35:38 +0530

    added new events

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -1,17 +1,17 @@
 import ComingSoonCard from "../../Components/ComingSoonCard/ComingSoonCard"
 import EventCard from "../../Components/EventCard/EventCard"
-import EventsData from "./data"
+import EventsData from "./data5.0"
 function Events() {
   return (
     <div className="relative mx-auto max-w-page_lg md:px-8 px-4 pt-32 overflow-x-hidden overflow-y-hidden">
       <div className="font-hero text-center font-semibold text-4xl">Events</div>
       <div className="mt-8">
-        {/* {EventsData.map((data, index) => {
+        {EventsData.map((data, index) => {
           return (
             <EventCard data={data} flipLayout={index % 2 === 1} key={index} />
           )
-        })} */}
-        <ComingSoonCard></ComingSoonCard>
+        })}
+        {/* <ComingSoonCard></ComingSoonCard> */}
       </div>
     </div>
   )
