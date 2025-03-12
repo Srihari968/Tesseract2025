f002e6146ef0241c623201075914c05ef2c605fe
Lataksh Sariya <latakshpatidar@gmail.com>
2025-03-13 00:16:42 +0530

    Changed the font of app bar and added the Tesseract logo with styling

--- src/Components/NavButton/NavButton.jsx
+++ src/Components/NavButton/NavButton.jsx
@@ -2,7 +2,7 @@ function NavButton({ content, isActive }) {
   return (
     <button
       type="button"
-      className={`py-2 px-4 font-medium font-sans rounded-full hover:bg-rose-600 hover:bg-opacity-10 ${
+      className={`py-2 px-4 font-medium rounded-full hover:bg-rose-600 hover:bg-opacity-10 ${
         isActive && "text-orange-600"
       }`}
     >
